8463395f01bdaaf04b06792ef8334da390c5fc3b
Alberto Arias <alberto@ajarias.dev>
2024-05-19 14:50:58 -0500

    document: currencyFormatter util function

--- client/src/shared/utils/index.ts
+++ client/src/shared/utils/index.ts
@@ -1,4 +1,13 @@
-export const currencyFormatter = (amount: number, currencyID: string) => {
+/**
+ * Formats the given amount in the specified currency.
+ *
+ * @param {number} amount - The numeric value to be formatted.
+ * @param {string} currencyID - The ISO 4217 currency code to be used for formatting.
+ *
+ * @returns {string} - The formatted amount in the specified currency.
+ */
+
+export const currencyFormatter = (amount: number, currencyID: string): string => {
   const formatter = new Intl.NumberFormat('es-AR', {
     style: 'currency',
     currency: currencyID,
